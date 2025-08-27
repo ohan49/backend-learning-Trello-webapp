6cc3589a1accc36e3736a114da41493eb6ba7022
AnhHao <pandalikesoda@gmail.com>
2025-08-27 16:09:49 +0700

    set up 1

--- src/config/environment.js
+++ src/config/environment.js
@@ -3,3 +3,12 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+import 'dotenv/config'
+
+export const env = {
+  MONGODB_URI: process.env.MONGODB_URI,
+  DATABASE_NAME: process.env.DATABASE_NAME,
+  APP_HOST: process.env.APP_HOST || 'localhost',
+  APP_PORT: process.env.APP_PORT || 8017,
+  AUTHOR: process.env.AUTHOR
+}
--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -4,4 +4,45 @@
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
 
+const MONGODB_URL =
+  'mongodb+srv://saopolip:Z4yIHxqySi98uUnn@cluster0.faurifr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
+const DATABASE_NAME = 'trello-demo-ah'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+//* Khởi tạo một đối tượng TrelloDatabaseInstance ban đầu là null
+let trelloDatabaseInstance = null
+
+//* Khởi tạo một đối tượng MongoClientInstance với URL kết nối MongoDB
+const mongoClientInstance = new MongoClient(MONGODB_URL, {
+  //? Lưu ý: serverApi có từ phiên bản 5.0.0 trở lên có thể không cần dùng nó, nếu dùng là chúng ta chỉ định một cái stable API version của MongoDB
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+//* Hàm kết nối đến cơ sở dữ liệu MongoDB
+export const CONNECT_DB = async () => {
+  //* gọi kết nối đến MongoDB
+  await mongoClientInstance.connect()
+
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+}
+
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
+}
+
+//* function GET_DB (Không async/await) có nhiệm vụ export ra cái Trello Database Instance sau khi đã connect thành công đến MongoDB để chúng ta sử dụng ở nhiều nơi khác nhau trong code.
+//* Lưu ý phải đảm bảo chỉ dùng gọi cái GET_DB này sau khi đã kết nối thành công tới MongoDB
+export const GET_DB = () => {
+  //* Kiểm tra xem trelloDatabaseInstance đã được khởi tạo chưa
+  if (!trelloDatabaseInstance)
+    throw new Error('Trello database instance is not initialized')
+
+  //* Trả về đối tượng trelloDatabaseInstance
+  return trelloDatabaseInstance
+}
