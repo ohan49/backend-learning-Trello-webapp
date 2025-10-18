750a6c789457c13d28727d4141bfab90c1206b3a
AnhHao <pandalikesoda@gmail.com>
2025-10-18 22:01:52 +0700

    update UX

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -83,7 +83,7 @@ const getDetails = async (id) => {
         }
       ])
       .toArray()
-    return result[0] || {}
+    return result[0] || null
   } catch (error) {
     throw new Error(error)
   }
@@ -96,4 +96,3 @@ export const boardModel = {
   findOneById,
   getDetails
 }
-
